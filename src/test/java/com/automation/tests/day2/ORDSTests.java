034652be8f52e7eb28da6d2df1066fd586dd3b13
pkouliev <pkouliev@gmail.com>
2020-05-19 18:16:11 -0400

    getAllEmployees method created

--- src/test/java/com/automation/tests/day2/ORDSTests.java
+++ src/test/java/com/automation/tests/day2/ORDSTests.java
@@ -1,5 +1,23 @@
 package com.automation.tests.day2;
 
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
 public class ORDSTests {
+
+    String Base_URL = "http://54.224.118.38:1000/ords/hr/";
+
+    @Test
+    @DisplayName("Get list of all employees")
+    public void getAllEmployees() {
+        Response response = given().
+                baseUri(Base_URL).
+                when().
+                get("/employees").prettyPeek();
+
+    }
 
 }
