41692983ac79dbbf9833efa01af4eeabc46ab8be
pkouliev <pkouliev@gmail.com>
2020-05-19 18:31:01 -0400

    Remarks and comments added

--- src/test/java/com/automation/tests/day2/ORDSTests.java
+++ src/test/java/com/automation/tests/day2/ORDSTests.java
@@ -13,11 +13,25 @@ public class ORDSTests {
     @Test
     @DisplayName("Get list of all employees")
     public void getAllEmployees() {
+
+        /**response can be saved in the Response object
+         * prettyPeek() - method that prints response info in nice format
+         *  also the method returns Response object
+         * response contains body, header and status line
+         * body (payload) contains content that we requested from the web service
+         * header - contains meta data
+         */
         Response response = given().
                 baseUri(Base_URL).
                 when().
                 get("/employees").prettyPeek();
 
+        /**
+         * RestAssured request has similar structure to BDD scenarios:
+         *
+         * given() - used for request setup and authentication
+         * when() - to specify type of HTTP request: get, put, post, delete, patch, head, etc...
+         * then() - to verify response, perform assertions
+         */
     }
-
 }
