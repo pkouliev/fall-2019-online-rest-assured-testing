fdda0578a0b478f5bc466136f49eb054efc33796
pkouliev <pkouliev@gmail.com>
2020-05-20 16:54:23 -0400

    addSpartan and deleteSpartan tests created

--- src/test/java/com/automation/tests/day2/SpartanTests.java
+++ src/test/java/com/automation/tests/day2/SpartanTests.java
@@ -1,7 +1,10 @@
 package com.automation.tests.day2;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
 
 import static io.restassured.RestAssured.given;
 
@@ -23,9 +26,57 @@ public class SpartanTests {
 
         given().
                 auth().basic("admin", "admin").
-                baseUri(Base_URL).
+                // authentication - who you are? you need to tell  to the server before getting any data
+                        baseUri(Base_URL).
                 when().
                 get("/api/spartans").prettyPeek().
                 then().statusCode(200);
+        // 200 - always after successful GET request
+    }
+
+    // add new spartant
+
+    @Test
+    @DisplayName("Add new spartan")
+    public void addSpartan() {
+        // JSON supports different data types: string, integer, boolean
+        String body = "{\"gender\": \"Male\", \"name\": \"Kirk Douglas\", \"phone\": 3012345678}";
+
+        // instead of string variable, we can use external JSON file
+        // use File class to read JSON and pass it into body
+        // provide path to the JSON as a parameter
+        File jsonFile = new File(System.getProperty("user.dir") + "/spartan.json");
+
+        //to create new item, we perform POST request
+        // contentType(ContentType.JSON) - to tell web service what kind of media type we send
+        given().
+                contentType(ContentType.JSON).
+                auth().basic("admin", "admin").
+                body(jsonFile).
+                baseUri(Base_URL).
+                when().
+                post("/api/spartans").prettyPeek().
+                then().statusCode(201);
+        // 201 - always for POST request
+
+    }
+
+    @Test
+    @DisplayName("Delete some spartan and verify status code is 356")
+    public void deleteSpartanTest() {
+
+        // {id} - path parameter
+        // YOU CANNOT DELETE SOMETHING TWICE
+        // we use delete() method to delete something
+        // 4XX - always after unsuccessful request and it was your FAULT
+        // ALL HTTP STATUS CODES HAVE SAME MEANING EVERYWHERE
+        given().
+                auth().basic("admin", "admin").
+                baseUri(Base_URL).
+                when().
+                delete("/api/spartans/{id}", 356).prettyPeek().
+                then().
+                statusCode(204);
+        // 204 - No content, most common status code for successful DELETE request
     }
 }
