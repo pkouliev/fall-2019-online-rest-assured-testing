48a3bf49e28049217d128d8f29178ea2c21daa09
pkouliev <pkouliev@gmail.com>
2020-05-23 12:35:51 -0400

    POJOPractice

---
+++ src/test/java/com/automation/tests/day5/POJOPractice.java
@@ -0,0 +1,23 @@
+package com.automation.tests.day5;
+
+import com.automation.utilities.ConfigurationReader;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class POJOPractice {
+
+    @BeforeAll
+    public static void beforeAll() {
+
+    }
+}
