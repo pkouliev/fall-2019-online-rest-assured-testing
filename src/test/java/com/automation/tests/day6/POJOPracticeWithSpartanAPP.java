d04910c8c5994efb5b0dd033f41a9631b6919ddc
pkouliev <pkouliev@gmail.com>
2020-05-23 22:49:05 -0400

    POJOPracticeWithSpartanAPP

---
+++ src/test/java/com/automation/tests/day6/POJOPracticeWithSpartanAPP.java
@@ -0,0 +1,34 @@
+package com.automation.tests.day6;
+
+import com.automation.pojos.Spartan;
+import com.automation.utilities.ConfigurationReader;
+import com.google.gson.Gson;
+import io.restassured.http.ContentType;
+import io.restassured.http.Headers;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.*;
+
+public class POJOPracticeWithSpartanAPP {
+
+    @BeforeAll
+    public static void beforeALL() {
+        baseURI = ConfigurationReader.getProperty("SPARTAN.URI");
+    }
+
+
+
+
+}
