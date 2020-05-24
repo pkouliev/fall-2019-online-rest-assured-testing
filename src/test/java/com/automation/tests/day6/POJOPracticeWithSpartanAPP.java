5de7b8ec11d1edc22d0bb510909e79e23f7ae283
pkouliev <pkouliev@gmail.com>
2020-05-24 10:58:49 -0400

    addSpartanTest test created

--- src/test/java/com/automation/tests/day6/POJOPracticeWithSpartanAPP.java
+++ src/test/java/com/automation/tests/day6/POJOPracticeWithSpartanAPP.java
@@ -7,12 +7,14 @@ import io.restassured.http.ContentType;
 import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -28,7 +30,45 @@ public class POJOPracticeWithSpartanAPP {
         baseURI = ConfigurationReader.getProperty("SPARTAN.URI");
     }
 
+    @Test
+    public void addSpartanTest() {
+        /**
+         * {
+         *     "gender": "Male",
+         *     "name": "Adam",
+         *     "phone": "8765432111"
+         * }
+         */
 
+        Map<String, String> spartan = new HashMap<>();
+        spartan.put("gender", "Male");
+        spartan.put("name", "Captain Kirk");
+        spartan.put("phone", "1234567890");
 
+        RequestSpecification requestSpecification = given().
+                auth().basic("admin", "admin").
+                contentType(ContentType.JSON).
+                accept(ContentType.JSON).
+                body(spartan);
+
+        Response response = given().
+                auth().basic("admin", "admin").
+                contentType(ContentType.JSON).
+                body(spartan).
+                when().
+                post("/spartans").prettyPeek();
+
+        response.then().statusCode(201);
+        response.then().body("success", is("A Spartan is Born!"));
+
+        // deserialization
+        Spartan spartanResponse = response.jsonPath().getObject("data", Spartan.class);
+        Map<String, Object> spartanResponseMap = response.jsonPath().getObject("data", Map.class);
+
+        System.out.println(spartanResponse);
+
+        // spartanResponse is a Spartan
+        System.out.println(spartanResponse instanceof Spartan); //must be true
+    }
 
 }
