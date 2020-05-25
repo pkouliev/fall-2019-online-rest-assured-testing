c444fca63449aedea2071efbe1fb92734a59b375
pkouliev <pkouliev@gmail.com>
2020-05-25 13:15:58 -0400

    Updated

--- src/test/java/com/automation/tests/day6/POJOPracticeWithORDS.java
+++ src/test/java/com/automation/tests/day6/POJOPracticeWithORDS.java
@@ -1,7 +1,10 @@
 package com.automation.tests.day6;
 
+import com.automation.pojos.Employee;
 import com.automation.utilities.ConfigurationReader;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,5 +15,12 @@ public class POJOPracticeWithORDS {
     @BeforeAll
     public static void setup() {
         baseURI = ConfigurationReader.getProperty("ORDS.URI");
+    }
+
+    @Test
+    public void getEmployeeTest() {
+        Response response = get("/employees/{id}", 100).prettyPeek();
+
+        Employee employee = response.as(Employee.class);
     }
 }
