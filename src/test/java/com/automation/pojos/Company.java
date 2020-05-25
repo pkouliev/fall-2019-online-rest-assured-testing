5c64b6ef86888c919d58a91c010f34b0837776d4
pkouliev <pkouliev@gmail.com>
2020-05-25 19:42:46 -0400

    Company

---
+++ src/test/java/com/automation/pojos/Company.java
@@ -0,0 +1,24 @@
+package com.automation.pojos;
+
+/**
+ * "company": {
+ * *         "companyId": 11783,
+ * *         "companyName": "Cybertek",
+ * *         "title": "SDET",
+ * *         "startDate": "02/01/2015",
+ * *         "address": {
+ * *             "addressId": 11803,
+ * *             "street": "123 Main Street",
+ * *             "city": "New York",
+ * *             "state": "NY",
+ * *             "zipCode": 10001
+ * *         }
+ */
+public class Company {
+    private int companyId;
+    private String companyName;
+    private String title;
+    private String startDate;
+
+
+}
