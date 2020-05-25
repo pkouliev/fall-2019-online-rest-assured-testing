05fa78bf3a72224991ad29d2375466456c23d4a3
pkouliev <pkouliev@gmail.com>
2020-05-25 19:43:23 -0400

    Student

---
+++ src/test/java/com/automation/pojos/Student.java
@@ -0,0 +1,53 @@
+package com.automation.pojos;
+
+/**
+ * {
+ * "studentId": 11843,
+ * "firstName": "Captain",
+ * "lastName": "Kirk",
+ * "batch": 15,
+ * "joinDate": "05/25/2015",
+ * "birthDate": "08/05/0191",
+ * "password": "4567",
+ * "subject": "string",
+ * "gender": "male",
+ * "admissionNo": "7897654",
+ * "major": "marineCyber",
+ * "section": "34567",
+ * "contact": {
+ * "contactId": 11863,
+ * "phone": "456-789-1234",
+ * "emailAddress": "apiCyber@cybertek.com",
+ * "premanentAddress": "Broadway, NYC"
+ * },
+ * "company": {
+ * "companyId": 11783,
+ * "companyName": "Cybertek",
+ * "title": "SDET",
+ * "startDate": "02/01/2015",
+ * "address": {
+ * "addressId": 11803,
+ * "street": "123 Main Street",
+ * "city": "New York",
+ * "state": "NY",
+ * "zipCode": 10001
+ * }
+ * }
+ */
+
+public class Student {
+    private int studentId;
+    private String firstName;
+    private String lastName;
+    private int batch;
+    private String joinDate;
+    private String birthDate;
+    private String password;
+    private String subject;
+    private String gender;
+    private String admissionNo;
+    private String major;
+    private String section;
+    private String contact;
+
+}
