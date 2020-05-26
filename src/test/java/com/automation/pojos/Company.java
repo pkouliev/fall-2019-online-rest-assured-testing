97db9848fdfe9aa8130f829eda531d99c142a099
pkouliev <pkouliev@gmail.com>
2020-05-25 20:09:09 -0400

    getters and setters added, toString generated

--- src/test/java/com/automation/pojos/Company.java
+++ src/test/java/com/automation/pojos/Company.java
@@ -19,6 +19,56 @@ public class Company {
     private String companyName;
     private String title;
     private String startDate;
+    private Address address; //Company has Address, does not extend Address
 
+    public int getCompanyId() {
+        return companyId;
+    }
 
+    public void setCompanyId(int companyId) {
+        this.companyId = companyId;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "companyId=" + companyId +
+                ", companyName='" + companyName + '\'' +
+                ", title='" + title + '\'' +
+                ", startDate='" + startDate + '\'' +
+                ", address=" + address +
+                '}';
+    }
 }
