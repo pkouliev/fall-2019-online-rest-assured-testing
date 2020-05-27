fba0e40dcf25b871b1e306f3beada9050e7de049
pkouliev <pkouliev@gmail.com>
2020-05-27 12:35:43 -0400

    Updated

--- src/test/java/com/automation/review/WeatherAPP.java
+++ src/test/java/com/automation/review/WeatherAPP.java
@@ -2,6 +2,9 @@ package com.automation.review;
 
 import io.restassured.response.Response;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Scanner;
 
 import static io.restassured.RestAssured.*;
@@ -25,12 +28,29 @@ public class WeatherAPP {
     public static String getWOEID(String city) {
         Response response = given().queryParam("query", city).get("/search");
         String woeid = response.jsonPath().getString("woeid");
-        System.out.println("WOEID :: " + woeid);
+//        System.out.println("WOEID :: " + woeid);
         return woeid;
     }
 
     public static void printWeatherInfo(String woeid) {
         woeid = woeid.replaceAll("\\D", ""); //to delete all non-digits
-        Response response = get("{woeid}", woeid).prettyPeek();
+        Response response = get("{woeid}", woeid);
+        List<String> weatherStateName = response.jsonPath().getList("consolidated_weather.weather_state_name");
+        List<Double> temp = response.jsonPath().getList("consolidated_weather.the_temp");
+        List<String> dates = response.jsonPath().getList("consolidated_weather.applicable_date");
+
+        System.out.println("Here is the weather forecast for this week");
+        for (int i = 0; i < weatherStateName.size(); i++) {
+            String date = dates.get(i);
+//            DateTimeFormatter.ofPattern("yyyy-MM-dd").parse(date);
+            date = LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd")).format((DateTimeFormatter.ofPattern("MMM dd, yyyy")));
+            System.out.printf("Date: %s, Weather state: %s, Temperature: %s\n", date, weatherStateName.get(i), temp.get(i));
+        }
+
+
+//        System.out.println(weatherStateName);
+//        System.out.println(temp);
+//        System.out.println(dates);
+
     }
 }
