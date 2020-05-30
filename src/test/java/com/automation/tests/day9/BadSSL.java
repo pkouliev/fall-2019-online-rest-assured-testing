d2a1ebaae0a54e6c9382320e2d33cd59026319cf
pkouliev <pkouliev@gmail.com>
2020-05-30 16:57:01 -0400

    Updated

--- src/test/java/com/automation/tests/day9/BadSSL.java
+++ src/test/java/com/automation/tests/day9/BadSSL.java
@@ -4,16 +4,22 @@ import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.Matchers.*;
 
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class BadSSL {
 
     @Test
-    public void badSSLCertificate() {
+    public void badSSLCertificateTest() {
         /**
          * no valid certificate - no handshake, no secure connection
+         * relaxedHTTPSValidation() - ignore SSL certificate issues
+         * Use relaxed HTTP validation with the given SSLContext protocol.
+         * This means that you'll trust all hosts regardless if the SSL certificate is invalid.
+         * By using this method you don't need to specify a keystore or trust store.
          */
         baseURI = "https://untrusted-root.badssl.com/";
-        get().prettyPeek();
+        Response response = given().relaxedHTTPSValidation().get().prettyPeek();
+        System.out.println(response.statusCode());
     }
 }
